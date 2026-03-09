e7908324c2e0f4c535cb343178b230ead649136e
Irina Ichim <onlyirina7@gmail.com>
2026-03-09 14:11:54 +0100

    fix: trigger redeploy to include web changes

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,3 +1,4 @@
+// Force redeploy - Update: Responsive & Social Security Fixes
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { NextFunction, Request, Response } from 'express';
